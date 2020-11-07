709a56bf7456b9007f49979a4e38caecaf8a867b
Ken Honda <ken@MacBook-Pro.local>
2020-11-07 23:39:08 +0900

    [add] first commit!

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h4>React Framework</h4>
+      <p>This is a React Framework</p>
+    </>
+  );
+}
+
+export default App;
