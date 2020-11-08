268de632a6a939d06c9e02abcb72124e8a5a62e7
Ken Honda <ken@MacBook-Pro.local>
2020-11-08 22:44:15 +0900

    [add] dashboard to framework

---
+++ src/screens/RootScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+
+const RootScreen = () => {
+    return (
+    <>
+    <h4>React Framework</h4>
+    <p>This is a React Framework</p>
+    <Button variant="contained" color="primary">sample button</Button>
+    </>
+    )
+};
+
+export default RootScreen;
